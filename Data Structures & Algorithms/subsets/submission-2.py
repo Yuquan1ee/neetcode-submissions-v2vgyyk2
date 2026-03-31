3fabafdba107cb3e5bc83f6aef065a36a5c71a00
Yu_quan <95670452+Yuquan1ee@users.noreply.github.com>
2026-03-31 19:21:45 +0800

    Add: subsets - submission-2

---
+++ Data Structures & Algorithms/subsets/submission-2.py
@@ -0,0 +1,24 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        # quantity of solution is 2**nums
+        answer = [[]]
+        for i in nums:
+            for x in range(len(answer)):
+                cur = answer[x].copy()
+                
+                cur.append(i)
+                answer.append(cur)
+        return answer
+            
+
+
+
+        
+
+
+
+
+
+
+        
+    
